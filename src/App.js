148d71ef2e1ff34dca8dd129c4b8ba851dfee24f
zhanghaowen <zhanghaowen@corp.netease.com>
2023-05-26 13:54:36 +0800

    feat: 初始化抽奖器

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React, { Component} from "react";
+import React, { useLayoutEffect, useState } from "react";
+import './css/style.css';
+import LotteryTiger from './tiger.js';
 
-class App extends Component{
-  render(){
+function App (){
+    const [lottery, setlottery] = useState(null);
+    const menu = [
+        '鱼香肉丝',
+        '肉末茄子',
+    ];
+
+    useLayoutEffect(() => {
+        const lottery = new LotteryTiger(document.getElementById('js_toggle'), document.querySelectorAll('.roller')) // eslint-disable-line
+        lottery.on('start',  () =>  {
+            setTimeout( () => {
+            var ret = [Math.round(Math.random() * 2)]
+            lottery.setResult(ret)
+        //            lottery.reset();
+            }, 1000)
+        })
+        setlottery(lottery);
+    }, [])
+
     return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
+        <div class="m-ui-tiger">
+            <div class="item">
+            <ul class="roller">
+                {
+                    menu.map((item) => {
+                        return (
+                            <li key={item}>
+                                {item}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            </div>
+            <a id="js_toggle" className="toggle" href="javascript:;" onClick={() => lottery?.draw()}></a>
+        </div>
     );
-  }
+  
 }
-
 export default App;
